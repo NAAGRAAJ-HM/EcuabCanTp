b59f70f2eeeaa78ceb8f279ab1909e6bf1651b30
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-02 11:39:43 +0000

    Update: AUTOSAR basic software ECU abstraction module for CAN communication protocol stack - transport layer

--- LATEST/CanTp.cpp
+++ LATEST/CanTp.cpp
@@ -83,12 +83,7 @@ FUNC(void, CANTP_CODE) module_CanTp::InitFunction(
    ){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_HIGH){
-            lptrCfg = lptrCfgModule;
-         }
-         else{
-            lptrCfg = &PBcfgCanTp;
-         }
+         lptrCfg = lptrCfgModule;
       }
       else{
 #if(STD_ON == CanTp_DevErrorDetect)
