9512eebe2fd5561ecd3d0226a04e7855f37d872f
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-04 19:55:24 +0200

    Update: AUTOSAR basic software ECU abstraction module for CAN communication protocol stack - transport layer

---
+++ main_LATEST/EcuabCanTp.cpp
@@ -0,0 +1,150 @@
+/******************************************************************************/
+/* File   : EcuabCanTp.cpp                                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "EcuabCanTp.hpp"
+#include "infEcuabCanTp_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CANTP_AR_RELEASE_VERSION_MAJOR                                         4
+#define CANTP_AR_RELEASE_VERSION_MINOR                                         3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(CANTP_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible CANTP_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(CANTP_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible CANTP_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_EcuabCanTp, CANTP_VAR) EcuabCanTp;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, CANTP_CODE) module_EcuabCanTp::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, CANTP_CONST,       CANTP_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   CANTP_CONFIG_DATA, CANTP_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == EcuabCanTp_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstEcuabCanTp_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == EcuabCanTp_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == EcuabCanTp_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == EcuabCanTp_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  CANTP_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, CANTP_CODE) module_EcuabCanTp::DeInitFunction(
+   void
+){
+#if(STD_ON == EcuabCanTp_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == EcuabCanTp_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == EcuabCanTp_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  CANTP_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, CANTP_CODE) module_EcuabCanTp::MainFunction(
+   void
+){
+#if(STD_ON == EcuabCanTp_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == EcuabCanTp_InitCheck)
+   }
+   else{
+#if(STD_ON == EcuabCanTp_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  CANTP_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, CANTP_CODE) module_EcuabCanTp::dummy(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
