2d152b1040003030fb86db66922be5a2ca89858d
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:05:08 +0200

    Update: AUTOSAR basic software ECU abstraction module for CAN communication protocol stack - transport layer

--- VirtualECU_LATEST/EcuabCanTp.cpp
+++ VirtualECU_LATEST/EcuabCanTp.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : EcuabCanTp.cpp                                                         */
+/* File   : EcuabCanTp.cpp                                                    */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define ECUABCANTP_AR_RELEASE_VERSION_MAJOR                                         4
-#define ECUABCANTP_AR_RELEASE_VERSION_MINOR                                         3
+#define ECUABCANTP_AR_RELEASE_VERSION_MAJOR                                    4
+#define ECUABCANTP_AR_RELEASE_VERSION_MINOR                                    3
 
 /******************************************************************************/
 /* MACROS                                                                     */
