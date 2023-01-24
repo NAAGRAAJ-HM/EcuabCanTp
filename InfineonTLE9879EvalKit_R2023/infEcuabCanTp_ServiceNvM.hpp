21eb1ff77c8e62b6ef00c5ed0e740d6ac08b2835
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:06 +0100

    Update: AUTOSAR basic software ECU abstraction module for CAN communication protocol stack - transport layer

---
+++ InfineonTLE9879EvalKit_R2023/infEcuabCanTp_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infEcuabCanTp_ServiceNvM.hpp                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTECUABCANTP_AR_RELEASE_VERSION_MAJOR                               4
+#define CONSTECUABCANTP_AR_RELEASE_VERSION_MINOR                               3
+
+#define Const_DefaultInit_ConstEcuabCanTp                                                                              \
+   ,  {CONSTECUABCANTP_AR_RELEASE_VERSION_MAJOR, CONSTECUABCANTP_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
